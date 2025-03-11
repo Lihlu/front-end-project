995a22bb4a39a2d2ba3993a5269a7c2a6070bb89
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-03-11 09:45:33 +0200

    Created actions for getting items by category

--- src/providers/foodItem/actions.tsx
+++ src/providers/foodItem/actions.tsx
@@ -5,6 +5,10 @@ export enum FoodItemActionEnums {
   getAllFoodItemsPending = "GET_ALL_FOOD_ITEMS_PENDING",
   getAllFoodItemsSuccess = "GET_ALL_FOOD_ITEMS_SUCCESS",
   getAllFoodItemsError = "GET_ALL_FOOD_ITEMS_ERROR",
+
+  getFoodItemsByCategoryPending = "GET_FOOD_ITEMS_BY_CATEGORY_PENDING",
+  getFoodItemsByCategorySuccess = "GET_FOOD_ITEMS_BY_CATEGORY_SUCCESS",
+  getFoodItemsByCategoryError = "GET_FOOD_ITEMS_BY_CATEGORY_ERROR",
 }
 
 export const getAllFoodItemsPending = createAction<IFoodItemStateContext>(
@@ -21,6 +25,23 @@ export const getAllFoodItemsSuccess = createAction<
   foodItems,
 }));
 export const getAllFoodItemsError = createAction<IFoodItemStateContext>(
+  FoodItemActionEnums.getAllFoodItemsError,
+  () => ({ isPending: false, isSuccess: false, isError: true })
+);
+
+export const getFoodItemsByCategoryPending =
+  createAction<IFoodItemStateContext>(
+    FoodItemActionEnums.getFoodItemsByCategoryPending,
+    () => ({ isPending: true, isSuccess: false, isError: false })
+  );
+
+export const getFoodItemsByCategorySuccess =
+  createAction<IFoodItemStateContext, IFoodItem[]>(
+    FoodItemActionEnums.getAllFoodItemsSuccess,
+    (foodItems: IFoodItem[]) => ({ isPending: false, isSuccess: true, isError: false, foodItems })
+  );
+
+export const getFoodItemsByCategoryError = createAction<IFoodItemStateContext>(
   FoodItemActionEnums.getAllFoodItemsError,
   () => ({ isPending: false, isSuccess: false, isError: true })
 );
