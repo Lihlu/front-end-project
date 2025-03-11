c15a9def5744e0f63cc9c0ef3126a9f4353e7f9b
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-03-11 13:07:04 +0200

    Added action to create food item to action context

--- src/providers/foodItem/context.tsx
+++ src/providers/foodItem/context.tsx
@@ -19,6 +19,21 @@ export interface IFoodItem{
     date: string
 }
 
+export interface IFoodItemInput{
+    name: string;
+    category: string;
+    servingSize: number;
+    protein: number;
+    carbs: number;
+    sugar: number;
+    fat: number;
+    fiber: number;
+    sodium: number;
+    potassium: number;
+    cholesterol: number;
+    energy: number;
+}
+
 export interface IFoodItemStateContext {
     isPending: boolean;
     isSuccess: boolean;
@@ -31,6 +46,7 @@ export interface IFoodItemActionContext {
     getAllFoodItems: (token: string) => void;
     getFoodItemsByCategory: (token: string, category: string) => void;
     getFoodItemsBySearch: (token: string, category: string) => void;
+    createFoodItem: (token: string, foodItemInput: IFoodItemInput) => void;
 }
 
 export const INITIAL_STATE: IFoodItemStateContext = {
