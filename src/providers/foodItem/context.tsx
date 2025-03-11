d605b7da981795591341381c7400305aa793e364
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-03-11 03:37:36 +0200

    Created food item contexts

---
+++ src/providers/foodItem/context.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { createContext } from "react";
+
+export interface IFoodItem{
+    _id: string;
+    name: string;
+    category: string;
+    servingSize: number;
+    protein: number;
+    carbs: number;
+    sugar: number;
+    fat: number;
+    fiber: number;
+    sodium: number;
+    potassium: number;
+    cholesterol: number;
+    energy: number;
+    date: string
+}
+
+export interface IFoodItemStateContext {
+    isPending: boolean;
+    isSuccess: boolean;
+    isError: boolean;
+    foodItem?: IFoodItem;
+    foodItems?: IFoodItem[];
+}
+
+export interface IFoodItemActionContext {
+    getAllFoodItems: (token: string) => void;
+}
+
+export const INITIAL_STATE: IFoodItemStateContext = {
+    isPending: false,
+    isSuccess: false,
+    isError: false,
+}
+
+export const FoodItemStateContext = createContext<IFoodItemStateContext>(INITIAL_STATE);
+
+export const FoodItemActionContext = createContext<IFoodItemActionContext>(undefined);
