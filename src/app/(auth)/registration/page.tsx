6a59aee6c4ae3a26d6b073e26d2c78bc93bad599
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-03-12 19:16:36 +0200

    Added trainer registration

---
+++ src/app/(auth)/registration/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button, Checkbox, Form, Input, Segmented } from "antd";
+import { useAuthActions } from "@/providers/auth";
+import { IRegistrationData } from "@/providers/auth/context";
+
+const RegistrationForm: React.FC = () => {
+  const [value, setValue] = useState("Trainer");
+  const { registerTrainer } = useAuthActions();
+
+  const onFinish = (values) => {
+    if (value === "Trainer") {
+      const registrationData: IRegistrationData = {
+        ...values,
+        role: "admin",
+        planType: "base",
+        activeState: true,
+        trial: false,
+      };
+
+      console.log(registrationData);
+      registerTrainer(registrationData);
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const handleSegmentedChange = (value: string) => {
+    setValue(value);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item label="Role:" name="role">
+        <Segmented
+          options={["Trainer", "Client"]}
+          value={value}
+          onChange={handleSegmentedChange}
+        />
+      </Form.Item>
+
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="email"
+        name="email"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Contact Number"
+        name="contactNumber"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Confirm Password"
+        name="confirmPassword"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item name="policiesAccepted" valuePropName="checked" label={null}>
+        <Checkbox>I accept MyGym's policies</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Create Account
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default RegistrationForm;
